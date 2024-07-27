b7ff5172a6515ba23278266f538f3f71a7c74267
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-27 22:13:43 +0800

    socket - improved socker_client

--- socket_test/socket_client.py
+++ socket_test/socket_client.py
@@ -4,8 +4,8 @@ client.connect(('192.168.68.64', 8000))
 
 # client.send(b"bobby")
 
-server_data = client.recv(1024)
-print("server response: {}".format(server_data.decode("utf8")))
+# server_data = client.recv(1024)
+# print("server response: {}".format(server_data.decode("utf8")))
 
 while True:
     input_data = input()
